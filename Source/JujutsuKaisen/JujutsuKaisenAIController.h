76d467730344ccb9252573818e4686c2103a4ebf
kim minsu <yaminsu5674@gmail.com>
2025-05-11 22:29:28 +0900

    feat: JujutsuKaisenAIController class

---
+++ Source/JujutsuKaisen/JujutsuKaisenAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "JujutsuKaisenAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JUJUTSUKAISEN_API AJujutsuKaisenAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
