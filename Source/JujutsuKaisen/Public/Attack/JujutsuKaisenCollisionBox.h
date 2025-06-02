9e5e44ab43cf184eef93a83d77bbc07da2890441
kim minsu <yaminsu5674@gmail.com>
2025-06-02 22:09:34 +0900

    feat: create JujutsuKaisenCollisionBox class

---
+++ Source/JujutsuKaisen/Public/Attack/JujutsuKaisenCollisionBox.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "JujutsuKaisenCollisionBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JUJUTSUKAISEN_API UJujutsuKaisenCollisionBox : public UBoxComponent
+{
+	GENERATED_BODY()
+	
+};
