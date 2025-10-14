7c71e37feda85b031017eabdac24a7a684cca9cd
kim minsu <yaminsu5674@gmail.com>
2025-10-14 18:48:06 +0900

    feat: CustomProjectileMovement enum class

--- Source/JujutsuKaisen/Private/Attack/CustomProjectileMovement.cpp
+++ Source/JujutsuKaisen/Private/Attack/CustomProjectileMovement.cpp
@@ -6,6 +6,7 @@ UCustomProjectileMovement::UCustomProjectileMovement()
 {
 	// 기본 설정
 	bNeverStop = false;
+	MoveType = EProjectileMoveType::Single; // 기본값: 단발형
 	
 	// 부모 클래스의 기본 설정 상속
 	PrimaryComponentTick.bCanEverTick = true;
@@ -14,7 +15,8 @@ UCustomProjectileMovement::UCustomProjectileMovement()
 void UCustomProjectileMovement::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	bNeverStop = true;
+	ProjectileGravityScale = 0.f;
 	// 초기화 로직
 	if (bNeverStop)
 	{
@@ -25,16 +27,116 @@ void UCustomProjectileMovement::BeginPlay()
 
 void UCustomProjectileMovement::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	// bNeverStop 모드일 때: 속도와 방향 저장
+	FVector SavedVelocity = Velocity;
+	FVector SavedDirection = Velocity.GetSafeNormal();
+	float SavedSpeed = Velocity.Size();
 	
-	// 커스텀 Tick 로직
+	// 0.5초 간격으로 Super::TickComponent 호출
+	static float AccumulatedTime = 0.0f;
+	AccumulatedTime += DeltaTime;
+	
+	if (AccumulatedTime >= 0.2f)
+	{
+		// 지역 변수로 복사
+		float CapturedTime = AccumulatedTime;
+		
+		// 람다로 Super::TickComponent 호출
+		auto CallSuperTick = [this, CapturedTime, TickType, ThisTickFunction]()
+		{
+			// 누적된 시간으로 한 번에 Tick
+			Super::TickComponent(CapturedTime, TickType, ThisTickFunction);
+			
+			if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Cyan, 
+					FString::Printf(TEXT("Super Tick Called! DeltaTime: %.3f"), CapturedTime));
+			}
+		};
+		
+		// 람다 실행
+		CallSuperTick();
+		
+		// 타이머 리셋
+		AccumulatedTime = 0.0f;
+	}
+	
+	// bNeverStop 모드: Super::TickComponent 이후 속도와 방향 강제 복원
 	if (bNeverStop)
 	{
-		// 속도가 0이 되지 않도록 유지
-		if (Velocity.IsNearlyZero())
+		// 중력 완전 비활성화
+		ProjectileGravityScale = 0.0f;
+		
+		// 속도와 방향 강제 복원 (중력이나 충돌로 변경된 것을 되돌림)
+		if (SavedSpeed > 0.0f)
 		{
-			Velocity = GetOwner()->GetActorForwardVector() * InitialSpeed;
+			Velocity = SavedDirection * InitialSpeed;
+			
+			if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 0.1f, FColor::Yellow, 
+					FString::Printf(TEXT("Velocity Locked: %s"), *Velocity.ToString()));
+			}
 		}
+	}
+}
+
+void UCustomProjectileMovement::HandleImpact(const FHitResult& Hit, float TimeSlice, const FVector& MoveDelta)
+{
+	// 충돌 디버그 메시지
+	if (Hit.GetActor())
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, TEXT("I got hit"));
+		}
+	}
+	
+	// 부모 클래스의 HandleImpact 호출 (기본 충돌 처리)
+	//Super::HandleImpact(Hit, TimeSlice, MoveDelta);
+}
+
+UCustomProjectileMovement::EHandleBlockingHitResult UCustomProjectileMovement::HandleBlockingHit(const FHitResult& Hit, float TimeTick, const FVector& MoveDelta, float& SubTickTimeRemaining)
+{
+	// 충돌해도 멈추지 않고 계속 진행
+	if (bNeverStop)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Magenta, TEXT("Blocking Hit Ignored - Continue!"));
+		}
+		// 방향 유지
+        Velocity = GetOwner()->GetActorForwardVector() * Velocity.Size();
+        ProjectileGravityScale = 0.f;
+		
+		// 충돌 무시하고 계속 진행
+		return EHandleBlockingHitResult::AdvanceNextSubstep;
+	}
+	
+	// 기본 동작 (멈춤)
+	return Super::HandleBlockingHit(Hit, TimeTick, MoveDelta, SubTickTimeRemaining);
+}
+
+void UCustomProjectileMovement::SetMoveType(EProjectileMoveType NewMoveType)
+{
+	MoveType = NewMoveType;
+	
+	// MoveType에 따라 설정 변경
+	switch (MoveType)
+	{
+	case EProjectileMoveType::Single:
+		// 단발형: 기본 설정
+		bNeverStop = false;
+		bShouldBounce = false;
+		bIsHomingProjectile = false;
+		break;
+		
+	case EProjectileMoveType::Rush:
+		// 돌진형: 멈추지 않음
+		bNeverStop = true;
+		bShouldBounce = false;
+		bIsHomingProjectile = false;
+		break;
 	}
 }
 
--- Source/JujutsuKaisen/Public/Attack/CustomProjectileMovement.h
+++ Source/JujutsuKaisen/Public/Attack/CustomProjectileMovement.h
@@ -6,6 +6,13 @@
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "CustomProjectileMovement.generated.h"
 
+UENUM(BlueprintType)
+enum class EProjectileMoveType : uint8
+{
+	Single,		// 단발형: 한 번 발사되어 날아가는 타입
+	Rush,		// 돌진형: 계속 추진력을 가지고 돌진하는 타입
+};
+
 /**
  * Custom Projectile Movement Component
  * ProjectileMovementComponent를 상속받아 커스텀 동작 구현
@@ -19,11 +26,26 @@ public:
 	UCustomProjectileMovement();
 
 protected:
+	// 발사체 이동 타입
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Custom Projectile")
+	EProjectileMoveType MoveType;
+
+protected:
 	// 부모 클래스의 가상 함수 오버라이드
 	virtual void BeginPlay() override;
 	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+	
+	// 충돌 처리 함수 오버라이드
+	virtual void HandleImpact(const FHitResult& Hit, float TimeSlice = 0.f, const FVector& MoveDelta = FVector::ZeroVector) override;
+	
+	// 블로킹 충돌 처리 오버라이드
+	virtual EHandleBlockingHitResult HandleBlockingHit(const FHitResult& Hit, float TimeTick, const FVector& MoveDelta, float& SubTickTimeRemaining) override;
 
 public:
+	// 이동 타입 설정 함수
+	UFUNCTION(BlueprintCallable, Category = "Custom Projectile")
+	void SetMoveType(EProjectileMoveType NewMoveType);
+	
 	// 충돌 시 멈추지 않고 계속 진행하도록 설정
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Custom Projectile")
 	bool bNeverStop;
